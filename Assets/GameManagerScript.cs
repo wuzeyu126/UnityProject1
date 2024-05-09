a8a8ea0f21ef872c11fd37684ed32a9ee4142b4e
wuzeyu126 <wuzeyu126@gmail.com>
2024-05-09 15:29:51 +0900

    01-02確認課題
    01-02確認課題

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -7,17 +7,54 @@ public class NewBehaviourScript : MonoBehaviour
     int[] map;
     string debugText = "";
 
+    void PrintArray()
+    {
+        string debugText = "";
+        for (int i = 0; i < map.Length; i++)
+        {
+            debugText += map[i].ToString() + ",";
+        }
+        Debug.Log(debugText);
+    }
+
+    int GetPlayerIndex()
+    {
+        for (int i = 0; i < map.Length; i++)
+        {
+            if (map[i] == 1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    bool MoveNumber(int number, int moveFrom, int moveTo)
+    {
+        if (moveTo < 0 || moveTo >= map.Length)
+        {
+            return false;
+        }
+        if (map[moveTo] == 2)
+        {
+            int velocity = moveTo - moveFrom;
+            bool success = MoveNumber(2, moveTo, moveTo + velocity);
+            if (!success)
+            {
+                return false;
+            }
+        }
+        map[moveTo] = number; ;
+        map[moveFrom] = 0;
+        return true;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        map = new int[] { 0, 0, 0, 1, 0, 0, 0, 0, 0 };
-        for (int i = 0; i < map.Length; i++)
-        {
-            //Debug.Log(map[i] + ",");
-            debugText += map[i].ToString() + ",";
-        }
-        // Debug.Log("Hello World");
-        Debug.Log(debugText);
+        map = new int[] { 0, 0, 0, 1, 0, 2, 0, 0, 0 };
+
+        PrintArray();
     }
 
     // Update is called once per frame
@@ -25,26 +62,10 @@ public class NewBehaviourScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            int playerIndex = -1;
-            for(int i = 0; i < map.Length; i++)
-            {
-                if (map[i] == 1)
-                {
-                    playerIndex = i;
-                    break;
-                }
-            }
-            if(playerIndex < map.Length - 1)
-            {
-                map[playerIndex + 1] = 1;
-                map[playerIndex] = 0;
-            }
-            string debugText = "";
-            for(int i = 0; i < map.Length; i++)
-            {
-                debugText += map[i].ToString() + ",";
-            }
-            Debug.Log(debugText);
+            int playerIndex = GetPlayerIndex();
+            MoveNumber(1, playerIndex, playerIndex + 1);
+            PrintArray();
         }
     }
+
 }
